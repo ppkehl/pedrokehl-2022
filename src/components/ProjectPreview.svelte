bad7138a01f0f00722093e07f1a7e095a69ddcf2
Pedro Kehl <ppkehl@gmail.com>
2022-07-01 10:31:17 -0300

    Netlify CMS integration

--- src/components/ProjectPreview.svelte
+++ src/components/ProjectPreview.svelte
@@ -1,23 +1,21 @@
 <script>
   export let project;
-  // console.log(project);
+  console.log(project.frontmatter);
 
-  const img = project.attributes.Cover.data.attributes.formats.hasOwnProperty('medium') ? project.attributes.Cover.data.attributes.formats.medium.url : project.attributes.Cover.data.attributes.url;
-  const alt = project.attributes.Cover.data.attributes.alternativeText;
-  const url = 'projects/' + project.attributes.slug;
+
 </script>
 
 <div class="w-full lg:w-4/12 mb-8 dark:text-white">
 
-  <a href="{url}" class="block space-y-5">
+  <a href="" class="block space-y-5">
     <div class="w-full aspect-video bg-slate-300">
-      <img class="object-cover w-full h-full" src="{img}" alt="{alt}">
+      <img class="object-cover w-full h-full" src="" alt="">
     </div>
     <h4 class="text-2xl font-semibold">
-      {project.attributes.Name}
+      {project.frontmatter.title}
     </h4>
     <div class="text-xl">
-      {project.attributes.Description}
+      {project.frontmatter.description}
     </div>
   </a>
 
