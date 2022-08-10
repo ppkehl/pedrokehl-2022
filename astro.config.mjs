dd3e56bd61356209ae1ac7864499a1716f5e49f8
Pedro Kehl <ppkehl@gmail.com>
2022-08-10 10:50:24 -0300

    Astro 1.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import preact from '@astrojs/preact'
+import partytown from "@astrojs/partytown";
+import image from "@astrojs/image";
 
 export default defineConfig({
-  integrations: [svelte(), tailwind(), sitemap()],
+  integrations: [svelte(), tailwind(), sitemap(), partytown(), image()],
   site: 'https://pedrokehl.net/'
 });
